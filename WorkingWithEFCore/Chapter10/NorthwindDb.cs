10188e229b5523dbefd930ae9a6e4c59fa6e2afe
aderosa <a.deosa@contrader.it>
2024-04-16 17:46:41 +0200

    second commit

--- WorkingWithEFCore/Chapter10/NorthwindDb.cs
+++ WorkingWithEFCore/Chapter10/NorthwindDb.cs
@@ -3,6 +3,9 @@ namespace Northwind.EntityModels;
 // This manages interactions with the Northwind database.
 public class NorthwindDb : DbContext
 {
+      // These two properties map to tables in the database.
+  public DbSet<Category>? Categories { get; set; }
+  public DbSet<Product>? Products { get; set; }
     protected override void OnConfiguring(
     DbContextOptionsBuilder optionsBuilder)
     {
@@ -12,4 +15,22 @@ public class NorthwindDb : DbContext
         WriteLine($"Connection: {connectionString}");
         optionsBuilder.UseSqlite(connectionString);
     }
+     protected override void OnModelCreating(
+    ModelBuilder modelBuilder)
+  {
+    // Example of using Fluent API instead of attributes
+    // to limit the length of a category name to 15.
+    modelBuilder.Entity<Category>()
+      .Property(category => category.CategoryName)
+      .IsRequired() // NOT NULL
+      .HasMaxLength(15);
+    // Some SQLite-specific configuration.
+    if (Database.ProviderName?.Contains("Sqlite") ?? false)
+    {
+      // To "fix" the lack of decimal support in SQLite.
+      modelBuilder.Entity<Product>()
+        .Property(product => product.Cost)
+        .HasConversion<double>();
+    }
+  }
 }
